898c87ae8596d3d6d2cf48d26f2e18d6858e5fb2
Anthony Halliday <anthony.halliday@outlook.com>
2017-02-15 22:03:57 +0100

    Configured web job deployment & added dependencies

--- NextBus.App/Program.cs
+++ NextBus.App/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿using OneSignal.CSharp.SDK;
+using OneSignal.CSharp.SDK.Resources;
+using OneSignal.CSharp.SDK.Resources.Notifications;
+using System;
+using System.Collections.Generic;
 
-class Program
+namespace NextBus.App
 {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            var client = new OneSignalClient("Yzg2MDViODAtNzE5OS00OTc0LTlmMmItNDhhYTYyOWVjZWE2");
 
+            var options = new NotificationCreateOptions()
+            {
+                AppId = new Guid("d88197f0-c247-4085-a72f-a7ac7aade44a"),
+                IncludedSegments = new List<string> { "All" },
+            };
+
+            options.Contents.Add(LanguageCodes.English, "Hello world!");
+
+            client.Notifications.Create(options);
+        }
     }
 }
